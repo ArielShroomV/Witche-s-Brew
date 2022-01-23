66a2010e94725347b11b50fb7212fff2be690381
Keshett <95711466+Keshett@users.noreply.github.com>
2022-01-23 22:24:20 +0200

    start sound
    added sound effect to start button

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioSource start;
+    public void PlayStart()
+    {
+        start.Play();
+    }
+}
