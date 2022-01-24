e75da80c8c3c10a02220b3e2e45ceea36fcde0a6
ArielShroomV <needanacc77@gmail.com>
2022-01-25 00:52:56 +0200

    Witch Animation movements
    added animations for walking and jumping!!! YAY

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,41 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+
 public class playerController : MonoBehaviour
 {
     public float movementSpeed = 4;
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _renderer;
     public float jumpForce = 5;
-
+    Animator anim;
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _renderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
 
     }
 
     private void Update()
     {
-        var movement = Input.GetAxis("Horizontal"); 
-        transform.position += new Vector3(movement,0,0) * Time.deltaTime * movementSpeed;
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+        Walk();
+        Jump();
+        Vector3 characterScale = transform.localScale;
+        transform.localScale = characterScale;
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+    }
+    private void Jump()
+    {
+        if (Input.GetButton("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
-        
-
-        Vector3 characterScale = transform.localScale;
-        if(Input.GetAxis("Horizontal") < 0)
+        if (_rigidbody.velocity.y != 0)
         {
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+    }
+
+    private void Walk()
+    {
+        var isJumping = anim.GetBool("isJumping");
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            anim.SetBool("isWalking", true && !isJumping);
             _renderer.flipX = true;
         }
-        if (Input.GetAxis("Horizontal") > 0)
+        else if (Input.GetAxis("Horizontal") > 0)
         {
+            anim.SetBool("isWalking", true && !isJumping);
             _renderer.flipX = false;
         }
-        transform.localScale = characterScale;
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
     }
+
 
 }
