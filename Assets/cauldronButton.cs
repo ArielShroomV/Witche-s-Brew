ce7b3d4b653427d8149575fee472c8b0e7f30ca0
ArielShroomV <needanacc77@gmail.com>
2022-02-04 15:08:43 +0200

    inventory & items (all levels)

---
+++ Assets/cauldronButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cauldronButton: MonoBehaviour
+{
+    private Inventory inventory;
+    private PickUp pickUp;
+    
+    void Cauldron()
+    {
+       // if inventory full...
+
+       
+    }
+
+
+    void Start()
+    {
+        Cauldron();
+    }
+
+    void Update()
+    {
+        
+    }
+}
