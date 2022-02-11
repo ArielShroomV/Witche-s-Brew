7b394196710fcc642dd081affe7ef2a2518ca146
Keshett <95711466+Keshett@users.noreply.github.com>
2022-02-11 22:04:41 +0200

    more work on dragon

--- Assets/Scripts/Dragon.cs
+++ Assets/Scripts/Dragon.cs
@@ -2,23 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+
 public class Dragon : MonoBehaviour
 {
    [SerializeField] private Animator animator;
     [SerializeField] private GameObject fireball;
     [SerializeField] private Transform mouth;
     [SerializeField] private GameObject projectile;
+    [SerializeField] private float speed;
+   [SerializeField] private Vector3[] positions;
+    private int index;
+    public Transform playerCharacter;
+    private SpriteRenderer spriteRenderer;
+    
+    
 
     public GameObject Projectile { get => projectile; set => projectile = value; }
 
-    void Start()
+
+
+    public void Awake()
     {
-        
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
     }
 
-   
+
     void Update()
     {
+        transform.position = Vector2.MoveTowards(transform.position, positions[index], Time.deltaTime * speed);
+        if (transform.position == positions[index])
+        {
+            if (index == positions.Length - 1)
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+        }
         
     }
     public void StopAttack()
@@ -31,4 +54,5 @@ public class Dragon : MonoBehaviour
         Vector3 direction = new Vector3(transform.localScale.x, 0);
         go.GetComponent<Projectile>().Setup(direction);
     }
+
 }
